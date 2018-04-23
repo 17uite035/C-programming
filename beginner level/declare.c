a5cda1095d18b902037eef29835b356dd415d40b
17uite035 <38652882+17uite035@users.noreply.github.com>
2018-04-23 09:41:06 +0000

    Create declare.c

---
+++ beginner level/declare.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	  char name[30]="Sachin Tendulkar";
+    char nicname[20]="Little Master";
+    int runs=18426;
+    float avg=44.83;
+    int odi=463;
+    printf("%s\n",name);
+    printf("%s\n",nicname);
+    printf("%d\n",runs);
+    printf("%.2f\n",avg);
+    printf("%d\n",odi);
+    return 0;
+}
+    
+
