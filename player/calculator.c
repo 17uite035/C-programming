66bb34e9cb7a946d66b5e0a3532a0d08f9d1f722
17uite035 <38652882+17uite035@users.noreply.github.com>
2018-04-24 04:44:29 +0000

    Create calculator.c

---
+++ player/calculator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int a=5;
+  int b=5;
+  int add,sub,multi,mod;
+  int divi;
+  add=a+b;
+  sub=a-b;
+  multi=a*b;
+  divi=a/b;
+  mod=a%b;
+  printf("%d,%d,%d,%d,%d",add,sub,multi,divi,mod);
+  return 0;
+}
+
