fc9f85ae14322a32a2df53088f12a7fddb5f8850
17uite035 <38652882+17uite035@users.noreply.github.com>
2018-06-19 10:15:15 +0000

    Update odd.c

--- codekata/odd.c
+++ codekata/odd.c
@@ -3,12 +3,12 @@ int main()
 {
     int N;
     scanf("%d",&N);
-    if(N%2==0)
+	if(N<0)
+		printf("invalid");
+    else if(N%2==0)
 	printf("Even");
     else if(N%2!=0)
      	printf("Odd");
-    else
-	printf("input is invalid");
       
     return 0;
 }
