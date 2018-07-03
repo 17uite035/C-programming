ea0f1b1d8e60dd882aba32d31bcb92db75ab5abe
17uite035 <38652882+17uite035@users.noreply.github.com>
2018-07-03 11:16:56 +0000

    Create arm.c

---
+++ codekata/arm.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int a,n,sum=0;
+	int t;
+	scanf("%d",&a);
+	t=a;
+	while(a>0)
+	{
+		n=a%10;
+		sum=sum+(n*n*n);
+		a=a/10;
+	}
+	if(t==sum)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+return 0;
+}
